76f71545f2a73f229f1090288acf507d889f71a8
Pointriangle <pointriangle@gmail.com>
2025-04-03 17:33:56 +0200

    v03041730

--- users.sql
+++ users.sql
@@ -1,6 +1,7 @@
 INSERT INTO users (pseudo, password, points, admin, created_at) 
 VALUES ('c', 'c', 0, 1, datetime('now')),
-('b', 'b', 0, 1, datetime('now'));
+('b', 'b', 0, 1, datetime('now')),
+('a', 'a', 0, 0, datetime('now'));
 
 INSERT INTO question (type,q) 
 VALUES ('genre','est ce un homme');
